df551202427b9a5de5a0451c162aeb923f0067c0
Abu Jehoiada <abujehoiada@gmail.com>
2024-03-27 18:21:49 -0700

    Fixed Pint Error

--- opcodes.c
+++ opcodes.c
@@ -70,7 +70,7 @@ void pint(stack_t **head, unsigned int line_number)
 {
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%u>: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*head)->n);
